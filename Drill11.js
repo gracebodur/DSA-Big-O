58c12367797201f30febcd31bb96bf49bb21125c
Grace <mgracebodur@gmail.com>
2020-03-11 23:46:07 -0400

    Initial commit

---
+++ Drill11.js
@@ -0,0 +1,7 @@
+// Derive an algorithm to solve the Tower of Hanoi puzzle.
+// Implement your algorithm using recursion. 
+// Your program should display each movement of the disk from one rod to another.
+
+// If you are given 5 disks, how do the rods look like after 7 recursive calls?
+// How many moves are needed to complete the puzzle with 3 disks? with 4 disks? with 5 disks?
+// What is the runtime of your algorithm?
---
+++ Drill12.js
@@ -0,0 +1,1 @@
+// Solve the drills 1 - 7 from your previous checkpoint (Recursion) iteratively.
